6bdceb1f92f5a749e68f6e1e8716c0339ab22d03
Ramen Biswas <ramen@devsramen.com>
2025-09-06 22:34:36 +0600

    Initial setup

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// Mongodb Connection
+mongoose
+  .connect(
+    "mongodb+srv://ramen:k6fptL17QZvCSO9G@oya.ecqa0f3.mongodb.net/oyaTodoList?retryWrites=true&w=majority&appName=OYA"
+  )
+  .then(() => {
+    console.log("Mongodb Connected");
+  });
+
+app.use(express.json());
+
 app.post("/registration", (req, res) => {
   res.send("Hello World!");
 });
